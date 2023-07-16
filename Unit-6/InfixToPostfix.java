dd61d96c5d5c959002c301d9763f1a8ba37c1707
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2023-07-16 18:40:35 +0530

    Update InfixToPostfix.java

--- Unit-6/InfixToPostfix.java
+++ Unit-6/InfixToPostfix.java
@@ -1,69 +1,69 @@
-import java.util.Stack;
+import java.util.*;
 
 public class InfixToPostfix {
-    
-    // Function to check if a given character is an operator
-    private static boolean isOperator(char ch) {
-        return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an infix string");
+        String s = sc.nextLine();
+        String postfix = convertInfixToPostfix(s);
+        System.out.println("Postfix string " + postfix);
+        sc.close();
     }
 
-    // Function to get the precedence of an operator
-    private static int getPrecedence(char operator) {
-        switch (operator) {
+    private static String convertInfixToPostfix(String s) {
+        String postfix = "";
+        Stack<Character> stack = new Stack<Character>();
+        stack.push('(');
+        s += ')';
+        int rank = 0;
+        for (char i : s.toCharArray()) {
+
+            if (Character.isLetterOrDigit(i)) {
+                postfix += i + " ";
+                rank++;
+            } else if (i == '(') {
+                stack.push(i);
+            } else if (i == ')') {
+                while (!stack.empty() && stack.peek() != '(') {
+                    postfix += stack.pop() + " ";
+                }
+                if (stack.empty() || stack.peek() != '(') {
+                    return "Invalid input string";
+                } else {
+                    stack.pop();
+                }
+            } else if (isOperator(i)) {
+                rank--;
+                while (!stack.empty() && getPrecedence(i) <= getPrecedence(stack.peek())) {
+                    postfix += stack.pop() + " ";
+                }
+                stack.push(i);
+            }
+        }
+
+        if (rank != 1)
+            return "Invalid input string";
+
+        return postfix;
+    }
+
+    private static int getPrecedence(char ch) {
+        switch (ch) {
             case '+':
             case '-':
                 return 1;
             case '*':
             case '/':
                 return 2;
+            case '^':
+            case '$':
+                return 3;
         }
         return -1;
     }
 
-    // Function to convert infix notation to postfix notation
-    private static String convertToPostfix(String infix) {
-        String postfix = "";
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < infix.length(); i++) {
-            char ch = infix.charAt(i);
-
-            if (Character.isLetterOrDigit(ch)) {
-                postfix += ch;
-            } else if (ch == '(') {
-                stack.push(ch);
-            } else if (ch == ')') {
-                while (!stack.isEmpty() && stack.peek() != '(') {
-                    postfix += stack.pop();
-                }
-
-                if (!stack.isEmpty() && stack.peek() != '(') {
-                    return "Invalid infix expression";
-                } else {
-                    stack.pop(); // Remove the '(' from the stack
-                }
-            } else if (isOperator(ch)) {
-                while (!stack.isEmpty() && getPrecedence(ch) <= getPrecedence(stack.peek())) {
-                    postfix += stack.pop();
-                }
-                stack.push(ch);
-            }
-        }
-
-        // Pop any remaining operators from the stack
-        while (!stack.isEmpty()) {
-            if (stack.peek() == '(') {
-                return "Invalid infix expression";
-            }
-            postfix += stack.pop();
-        }
-
-        return postfix;
+    private static boolean isOperator(char ch) {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^' || ch == '$';
     }
 
-    public static void main(String[] args) {
-        String infixExpression = "(A + B * C / D - E + F / G / ( H + I ))";
-        String postfixExpression = convertToPostfix(infixExpression);
-        System.out.println("Postfix notation: " + postfixExpression);
-    }
 }
